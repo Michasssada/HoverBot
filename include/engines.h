f6eeb69f8c35c842d8e53f04fc357b6fa6782437
Michasssada <michaslate@gmail.com>
2024-10-07 14:23:52 +0200

    modified:   include/engines.h modified:   src/engines.cpp

--- include/engines.h
+++ include/engines.h
@@ -4,10 +4,12 @@
 #include <softPwm.h>
 class Engines{
     public:
-       Engines(int Pin);
-       void engine_write(int speed);
+       Engines(int Pin,int Dir_pin);
+       void engine_write(int speed,bool Direction);//0-100 engine power, forward is false backward is true
+       void stop();
     private:
         int pin;
+        int dir;
     
 };
 #endif
--- src/engines.cpp
+++ src/engines.cpp
@@ -1,6 +1,6 @@
 #include "engines.h"
 #include <iostream>
-Engines::Engines(int Pin): pin(Pin){
+Engines::Engines(int Pin, int Dir_pin): pin(Pin), dir(Dir_pin){
     if (wiringPiSetupGpio() == -1) {
         std::cerr << "Failed to initialize WiringPi" << std::endl;
     }
@@ -8,8 +8,18 @@ Engines::Engines(int Pin): pin(Pin){
     
 
 }
-void Engines::engine_write(int speed){
+void Engines::engine_write(int speed, bool Direction){
+    if(Direction){
+        Wir
+    }
+    else{
+
+    }
     softPwmWrite(pin, speed);
+
+}
+void Engines::stop(){
+    softPwmWrite(pin, 0);
 
 }
 
